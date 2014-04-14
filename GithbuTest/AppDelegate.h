288d5b6d22e238c5aed46a9ec114925e730b28d9
yong <yongjoon0726@gmail.com>
2014-04-14 11:32:49 -0700

    Initial Commit

---
+++ GithbuTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  GithbuTest
+//
+//  Created by yong choi on 2014. 4. 14..
+//  Copyright (c) 2014년 whispr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ GithbuTest/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  GithbuTest
+//
+//  Created by yong choi on 2014. 4. 14..
+//  Copyright (c) 2014년 whispr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end

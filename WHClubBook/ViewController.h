45886308aad45368df7e5ddf894102873270bd49
yong <yongjoon0726@gmail.com>
2014-04-23 11:30:33 -0700

    Initial Commit

---
+++ WHClubBook/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  WHClubBook
+//
+//  Created by yong choi on 2014. 4. 23..
+//  Copyright (c) 2014년 whispr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ WHClubBook/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  WHClubBook
+//
+//  Created by yong choi on 2014. 4. 23..
+//  Copyright (c) 2014년 whispr. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end

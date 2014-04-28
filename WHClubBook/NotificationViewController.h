2b659cff42cc73a6fda76d5965134cdc9a35d1b7
yong <yongjoon0726@gmail.com>
2014-04-28 15:30:42 -0700

    practice

---
+++ WHClubBook/NotificationViewController.h
@@ -0,0 +1,13 @@
+//
+//  NotificationViewController.h
+//  WHClubBook
+//
+//  Created by yong choi on 2014. 4. 28..
+//  Copyright (c) 2014년 whispr. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NotificationViewController : UIViewController
+
+@end
